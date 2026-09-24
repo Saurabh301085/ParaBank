b8fb0dc081a66f962d1f08bc1a44186e0b248840
Saurabh301085 <saurabhk301085@outlook.com>
2026-09-24 15:42:47 +0530

    change in the file name of main test file

--- Utilities/logger.py
+++ Utilities/logger.py
@@ -1,3 +1,29 @@
+# import logging
+# import os
+#
+#
+# def get_logger():
+#     logger = logging.getLogger(__name__)
+#     logger.setLevel(logging.INFO)
+#
+#     # Get Parabank project folder path
+#     project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+#     # Create log file path
+#     log_path = os.path.join(project_path, "logs", "banklog.log")
+#
+#     file_handler = logging.FileHandler(log_path)
+#
+#     formatter = logging.Formatter(
+#         "%(asctime)s - %(levelname)s - %(message)s"
+#     )
+#
+#     file_handler.setFormatter(formatter)
+#
+#     logger.addHandler(file_handler)
+#
+#     return logger
+
 import logging
 import os
 
@@ -6,20 +32,10 @@ def get_logger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    # Get Parabank project folder path
     project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    # Create log file path
     log_path = os.path.join(project_path, "logs", "banklog.log")
-
-    file_handler = logging.FileHandler(log_path)
-
-    formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(message)s"
-    )
-
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-
+    filehandler = logging.FileHandler(log_path)
+    file_formatter = logging.Formatter("%(asctime)s - %(levelname)s -%(message)s,")
+    filehandler.setFormatter(file_formatter)
+    logger.addHandler(filehandler)
     return logger
